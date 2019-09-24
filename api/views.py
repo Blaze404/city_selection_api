5864830bf74f5722c966cb7b024bd37e83c76a24
Blaze404 <mus.qazi999@gmail.com>
2019-09-25 00:52:56 +0530

    csrf excepmt added

--- api/views.py
+++ api/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Countries
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
 def index(request):
     return None
 
-
+@csrf_exempt
 def country(request):
     if request.method == 'POST':
         return JsonResponse(Countries.objects.all())
 
+@csrf_exempt
 def state(request):
     pass
 
+@csrf_exempt
 def city(request):
     pass
